b425cc4d02928558c965580555b539b75974ff0d
carla <carlajilo@gmail.com>
2024-05-18 12:41:41 +0200

    New patient

--- models/PacienteModel.php
+++ models/PacienteModel.php
@@ -32,5 +32,32 @@ class PacienteModel extends Basedatos {
             return 'Error al devolver las citas.<br>'. $e->getMessage();
         }
     }
+    
+    /**
+     * New Patient
+     * @param type $post
+     * @return string
+     */
+    public function RecordPatient($post) {
+        try {
+            $sql = `insert into $this->table ('DNI', 'nombre', 'apellidos', 'direccion', 'telefono', 'email', 'password') values (?, ?, ?, ?, ?, ?, ?)`;
+            $sentencia = $this->conexion->prepare($sql);
+            $sentencia->bindParam(1, $post['DNI']);
+            $sentencia->bindParam(2, $post['nombre']);
+            $sentencia->bindParam(3, $post['apellidos']);
+            $sentencia->bindParam(4, $post['direccion']);
+            $sentencia->bindParam(5, $post['telefono']);
+            $sentencia->bindParam(6, $post['email']);
+            $sentencia->bindParam(7, $post['password']);
+            $insert = $sentencia->execute();
+            
+            $mensaje = "";
+            $mensaje = "Se ha registrado correctamente";
+            return $mensaje;
+            
+        } catch (PDOException $e) {
+            return 'Error al registrarse.<br>'. $e->getMessage();
+        }
+    }
 }
 
